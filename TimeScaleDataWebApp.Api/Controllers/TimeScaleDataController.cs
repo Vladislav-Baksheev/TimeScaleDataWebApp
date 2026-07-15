a93c7073ba9d7fb9d2c5c6475367f3398b75793e
Владислав Бакшеев <bogomolleha7@gmail.com>
2026-07-15 17:20:43 +0700

    initial commit

---
+++ TimeScaleDataWebApp.Api/Controllers/TimeScaleDataController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimeScaleDataWebApp.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TimeScaleDataController : ControllerBase
+{
+    
+}
