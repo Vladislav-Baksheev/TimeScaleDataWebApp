98558b6f88fd65a6869ca307da7dda03a68f3b40
Владислав Бакшеев <bogomolleha7@gmail.com>
2026-07-20 23:30:03 +0700

    Добавлен переход по методам

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { UploadComponent } from './upload/upload';
+import { ValuesComponent } from './values/values';
+import { ResultsComponent } from './results/results';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'upload',
+    component: UploadComponent
+  },
+
+  {
+    path: 'filter',
+    component: ResultsComponent
+  },
+
+  {
+    path: 'values',
+    component: ValuesComponent
+  }
+];
--- angular-app/src/app/app.ts
+++ angular-app/src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { UploadComponent } from './upload/upload';
 import {ValuesComponent} from './values/values'
 import { ResultsComponent } from "./results/results";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UploadComponent, ValuesComponent, ResultsComponent],
+  imports: [
+    RouterOutlet,
+    UploadComponent,
+    ValuesComponent,
+    ResultsComponent,
+    RouterLink
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
