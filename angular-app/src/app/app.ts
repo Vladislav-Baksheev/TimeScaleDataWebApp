12821ca5c5776d7c436affcd86bb40c658fd5adb
Владислав Бакшеев <bogomolleha7@gmail.com>
2026-07-20 22:11:14 +0700

    Реализована загрузка файла и отображение значений по имени файла

--- angular-app/src/app/app.ts
+++ angular-app/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UploadComponent } from './upload/upload';
+import {ValuesComponent} from './values/values'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UploadComponent, ValuesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-app');
+
 }
