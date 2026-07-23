b9c087f592d08177c709c197ce64f789ca028c01
Владислав Бакшеев <bogomolleha7@gmail.com>
2026-07-23 21:44:24 +0700

    Добавлены миграции

---
+++ TimeScaleDataWebApp.Infrastructure/Migrations/20260716134934_Initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TimeScaleDataWebApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "results",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    delta_seconds = table.Column<double>(type: "double precision", nullable: false),
+                    start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    avg_execution_time = table.Column<double>(type: "double precision", nullable: false),
+                    avg_value = table.Column<double>(type: "double precision", nullable: false),
+                    median_value = table.Column<double>(type: "double precision", nullable: false),
+                    max_value = table.Column<double>(type: "double precision", nullable: false),
+                    min_value = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_results", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "values",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    execution_time = table.Column<float>(type: "real", nullable: true),
+                    value = table.Column<float>(type: "real", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_values", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "results");
+
+            migrationBuilder.DropTable(
+                name: "values");
+        }
+    }
+}
---
+++ TimeScaleDataWebApp.Infrastructure/Migrations/20260716214158_AddFileName.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeScaleDataWebApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFileName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "value",
+                table: "values",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "execution_time",
+                table: "values",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "date",
+                table: "values",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "values",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "results",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "values");
+
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "results");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "value",
+                table: "values",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "execution_time",
+                table: "values",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "date",
+                table: "values",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
